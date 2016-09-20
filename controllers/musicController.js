4c5fb0eeb2eb112742b25faa2808393250a0bebb
Antonio Fontaine <afontaine@uc.cl>
2016-09-20 07:29:52 -0300

    listen part working, administration still missing

---
+++ controllers/musicController.js
@@ -0,0 +1,42 @@
+const fs = require('fs');
+const path = require('path');
+
+
+
+exports.getSong = function(req, res) {
+    let path = "music/" + req.params.directory;
+
+    console.log(path);
+    fs.readdir(path, function(err, items) {
+        if (err) {
+            return res.status(404).send({
+                error: "directory does not exists"
+            });
+        }
+        let index = Math.floor((Math.random() * items.length));
+        let song = items[index];
+        var stream = fs.createReadStream(path + "/" + song);
+        stream.pipe(res);
+
+    });
+};
+
+
+exports.home = function(req, res) {
+
+    let path = "music";
+
+    console.log(path);
+    fs.readdir(path, function(err, items) {
+        if (err) {
+            return res.status(404).send({
+                error: "directory does not exists"
+            });
+        }
+        console.log(items);
+        res.render('./index.ejs', {
+            directories: items
+        });
+    });
+
+};
---
+++ routes/index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+const musicController = require('../controllers/musicController');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  musicController.home(req, res);
+});
+
+router.get('/admin', function(req, res, next) {
+
+});
+
+router.get('/listen/:directory', function(req, res, next) {
+musicController.getSong(req, res);
+})
+
+
+
+module.exports = router;
